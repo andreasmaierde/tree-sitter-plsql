84b25572aa26759495cc4e83791586e6cca5fd9c
Andreas Maier <andreas.maier.de@gmail.com>
2023-01-06 23:52:16 +0100

    add some drop examples

---
+++ examples/drop_object.sql
@@ -0,0 +1,30 @@
+/*-----------------------------------------------------------------------------------------------
+|@|description Simple Examples DROP Objects
+*/-----------------------------------------------------------------------------------------------
+
+DROP TRIGGER myschema.mytrigger;
+DROP TRIGGER mytrigger;
+
+DROP FUNCTION myfunc;
+DROP function myschema.myfunc;
+
+DROP PROCEDURE myproc;
+DROP PROCEDURE myschema.myproc;
+
+DROP library mylib;
+
+DROP PACKAGE myschema.mypackage;
+DROP PACKAGE mypackage;
+
+DROP PACKAGE BODY myschema.mypackage;
+DROP PACKAGE BODY mypackage;
+
+DROP trigger mytrigger;
+DROP trigger myschema.mytrigger;
+
+DROP type myschema.mytype FORCE;
+DROP type myschema.mytype VALIDATE;
+DROP type myschema.mytype;
+
+DROP type body myschema.mytype;
+DROP type body mytype;
